be2f8d306ff8b2d3c54ab4703b4ee0200337d6ce
John Johnson <johnrjj@gmail.com>
2016-11-24 11:01:20 -0500

    Project(s) now optional, issue type supports space(s) in the names

--- src/lib/components/query-builder.spec.ts
+++ src/lib/components/query-builder.spec.ts
@@ -4,7 +4,6 @@ import { buildJiraSearchQueryUrl } from './query-builder';
 
 describe('query builder', () => {
     it('should build query from multiple entry arrays', () => {
-
         const endDate = new Date(2016, 11, 26);
         const startDate = new Date(2016, 11, 13);
 
@@ -20,11 +19,51 @@ describe('query builder', () => {
             startIndex: 0,
         });
 
-        const actual = 'https://APIROOTURL/rest/api/latest/search?jql=project%20in%20(TESTPROJECT1%2CTESTPROJECT2)%20AND%20issuetype%20in%20(ISSUE_TYPE1%2CISSUE_TYPE2)%20AND%20((resolutionDate%20%3E%3D%20%222016%2F12%2F13%22%20OR%20resolution%20%3D%20Unresolved)%20OR%20(resolutionDate%20%3C%3D%20%222016%2F12%2F26%22))%20AND%20filter%3D%22a%22%20AND%20filter%3D%22b%22%20AND%20filter%3D%22c%22%20order%20by%20key&startAt=0&maxResults=25&expand=changelog';
+        const actual = 'https://APIROOTURL/rest/api/latest/search?jql=project%20in%20(TESTPROJECT1%2CTESTPROJECT2)%20AND%20issuetype%20in%20(%22ISSUE_TYPE1%22%2C%22ISSUE_TYPE2%22)%20AND%20((resolutionDate%20%3E%3D%20%222016%2F12%2F13%22%20OR%20resolution%20%3D%20Unresolved)%20OR%20(resolutionDate%20%3C%3D%20%222016%2F12%2F26%22))%20AND%20filter%3D%22a%22%20AND%20filter%3D%22b%22%20AND%20filter%3D%22c%22%20order%20by%20key&startAt=0&maxResults=25&expand=changelog';
 
         expect(builtSearchQueryUrl).to.equal(actual);
     });
 
+    it('should allow for project to be optional', () => {
+        const endDate = new Date(2016, 11, 26);
+        const startDate = new Date(2016, 11, 13);
+
+        const builtSearchQueryUrl = buildJiraSearchQueryUrl({
+            apiRootUrl: 'https://APIROOTURL',
+            batchSize: 25,
+            customJql: '',
+            endDate: endDate,
+            filters: ['a'],
+            issueTypes: ['ISSUE_TYPE1'],
+            projects: [],
+            startDate: startDate,
+            startIndex: 0,
+        });
+
+        const actual = 'https://APIROOTURL/rest/api/latest/search?jql=issuetype%20in%20(%22ISSUE_TYPE1%22)%20AND%20((resolutionDate%20%3E%3D%20%222016%2F12%2F13%22%20OR%20resolution%20%3D%20Unresolved)%20OR%20(resolutionDate%20%3C%3D%20%222016%2F12%2F26%22))%20AND%20filter%3D%22a%22%20order%20by%20key&startAt=0&maxResults=25&expand=changelog';
+
+        expect(builtSearchQueryUrl).to.equal(actual);
+    });
+    it('should support issue types with spaces', () => {
+        const endDate = new Date(2016, 11, 26);
+        const startDate = new Date(2016, 11, 13);
+
+        const builtSearchQueryUrl = buildJiraSearchQueryUrl({
+            apiRootUrl: 'https://APIROOTURL',
+            batchSize: 25,
+            customJql: '',
+            endDate: endDate,
+            filters: ['a'],
+            issueTypes: ['ISSUE_TYPE WITH SPACES'],
+            projects: ['TESTPROJECT'],
+            startDate: startDate,
+            startIndex: 0,
+        });
+
+        const actual = 'https://APIROOTURL/rest/api/latest/search?jql=project%3DTESTPROJECT%20AND%20issuetype%20in%20(%22ISSUE_TYPE%20WITH%20SPACES%22)%20AND%20((resolutionDate%20%3E%3D%20%222016%2F12%2F13%22%20OR%20resolution%20%3D%20Unresolved)%20OR%20(resolutionDate%20%3C%3D%20%222016%2F12%2F26%22))%20AND%20filter%3D%22a%22%20order%20by%20key&startAt=0&maxResults=25&expand=changelog';
+
+        expect(builtSearchQueryUrl).to.equal(actual);
+    });
     it('should build query from single entry arrays', () => {
 
         const endDate = new Date(2016, 11, 26);
@@ -42,7 +81,7 @@ describe('query builder', () => {
             startIndex: 0,
         });
 
-        const actual = 'https://APIROOTURL/rest/api/latest/search?jql=project%3DTESTPROJECT%20AND%20issuetype%20in%20(ISSUE_TYPE1)%20AND%20((resolutionDate%20%3E%3D%20%222016%2F12%2F13%22%20OR%20resolution%20%3D%20Unresolved)%20OR%20(resolutionDate%20%3C%3D%20%222016%2F12%2F26%22))%20AND%20filter%3D%22a%22%20order%20by%20key&startAt=0&maxResults=25&expand=changelog';
+        const actual = 'https://APIROOTURL/rest/api/latest/search?jql=project%3DTESTPROJECT%20AND%20issuetype%20in%20(%22ISSUE_TYPE1%22)%20AND%20((resolutionDate%20%3E%3D%20%222016%2F12%2F13%22%20OR%20resolution%20%3D%20Unresolved)%20OR%20(resolutionDate%20%3C%3D%20%222016%2F12%2F26%22))%20AND%20filter%3D%22a%22%20order%20by%20key&startAt=0&maxResults=25&expand=changelog';
 
         expect(builtSearchQueryUrl).to.equal(actual);
     });
